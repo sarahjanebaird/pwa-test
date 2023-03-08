124ab520766b63f73fdd834e8f1ccf17715a5bb8
sarahjanebaird <sarah.baird@colibri-software.com>
2023-03-08 13:02:22 -0400

    fix?

--- vue.config.js
+++ vue.config.js
@@ -7,12 +7,5 @@ module.exports = defineConfig({
     msTileColor: "#000000",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
-    // configure the workbox plugin
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: "./src/service-worker.js",
-      // ...other Workbox options...
   },
-  }
 })
